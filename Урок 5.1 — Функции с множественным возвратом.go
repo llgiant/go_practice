6bea74d4bd64d5ff7b4c71251b09ad77ebe1a8d3
Bogdan Miller <llgiant@gmail.com>
2025-08-12 21:21:56 +0600

    Add min-max function to handle multiple return values
    This commit introduces a `findMinMax` function that calculates the minimum and maximum values of a slice. Includes error handling for empty slices and updated the `main` function with examples.

--- Урок 5.1 — Функции с множественным возвратом.go
+++ Урок 5.1 — Функции с множественным возвратом.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+func findMinMax(values []int) (int, int) {
+	if len(values) == 0 {
+		fmt.Println("Срез пустой!")
+		return 0, 0
+	}
+
+	min, max := values[0], values[0]
+
+	for _, value := range values[1:] {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+	return min, max
+}
+
+func main() {
+	//values := []int{3, 8, 2, 5, 1, 9}
+	values := []int{} // ← попробуй с пустым срезом
+
+	min, max := findMinMax(values)
+	fmt.Printf("Минимум: %d\n", min)
+	fmt.Printf("Максимум: %d\n", max)
 }
