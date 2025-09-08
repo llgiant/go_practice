8232832ae9deeac0495d3c126b99aba316c8617c
Bogdan Miller <llgiant@gmail.com>
2025-09-08 22:07:37 +0600

    Add word count program and text file for file processing example
    Introduced two Go programs: one for counting words in a file (`text.txt`) with file reading and text processing, and the other as a placeholder for writing to a file. Also added a sample text file with content for testing.

---
+++ Stage-9-working-with-files/Задание-9-1—Подсчёт-слов-в-файле.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("text.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var words []string
+
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		words = append(words, fields...) //why nil?
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Количество слов: %d\n", len(words))
+	for _, word := range words {
+		fmt.Println(word)
+	}
+}
