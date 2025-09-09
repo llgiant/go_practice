b03cab773f811102aa75beb25df408d3706f98f5
Miller <bmelnikovskiy@ad.salyk.kg>
2025-09-09 10:49:24 +0600

    Add word write into the file

--- Stage-9-working-with-files/Этап 9.2 — Запись в файл.go
+++ Stage-9-working-with-files/Этап 9.2 — Запись в файл.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("text.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var words []string
+
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		words = append(words, fields...)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	uniqueMap := make(map[string]struct{})
+
+	for _, word := range words {
+		uniqueMap[word] = struct{}{}
+	}
+
+	uniqueWords := make([]string, 0, len(uniqueMap))
+
+	for word := range uniqueMap {
+		uniqueWords = append(uniqueWords, word)
+	}
+
+	sort.Strings(uniqueWords)
+
+	outputFile, err := os.Create("words.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	writer := bufio.NewWriter(outputFile)
+	for _, word := range uniqueWords {
+		_, err := writer.WriteString(word + "\n")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	writer.Flush()
+
+	if err := writer.Flush(); err != nil {
+		panic(err)
+	}
+}
